dc00553913617d38a9e880a9459076de0e0f377c
Hugo Latendresse <hugolatendresse@gmail.com>
2024-01-01 19:15:29 -0600

    needed input

--- prework/learn-c.c
+++ prework/learn-c.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int main() {
-  
-  char c;
-  c = '?';
-
-  printf("%d", c);
-
-
-
-  return 0;
+int main()
+{
+	float v1, v2;
+	
+	scanf("%f", &v1);
+	scanf("%f", &v2);
+	
+	printf("Value A: %.1f\n", v1);
+	printf("Value B: %.1f\n", v2);
+	printf("%.5f + %.5f = %.5f\n",v1,v2, v1+v2);
+	printf("%.5f - %.5f = %.5f\n",v1,v2, v1-v2);
+	printf("%.5f * %.5f = %.5f\n",v1,v2, v1*v2);
+	
+	
+	return 0;
 }
