d54ba73dfdf77a6f6720fcd197a506c4e880417e
Hugo Latendresse <hugolatendresse@gmail.com>
2024-01-02 20:59:04 -0600

    strspn

--- prework/learn-c.c
+++ prework/learn-c.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
-	float v1, v2;
-	
-	scanf("%f", &v1);
-	scanf("%f", &v2);
-	
-	printf("Value A: %.1f\n", v1);
-	printf("Value B: %.1f\n", v2);
-	printf("%.5f + %.5f = %.5f\n",v1,v2, v1+v2);
-	printf("%.5f - %.5f = %.5f\n",v1,v2, v1-v2);
-	printf("%.5f * %.5f = %.5f\n",v1,v2, v1*v2);
-	
-	
-	return 0;
+  char* laureate = "Barry Sharpless";
+  int length = strspn(laureate + 1, "road");
+  printf("%d", length);
+
 }
